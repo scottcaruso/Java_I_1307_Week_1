d9f9161c041bd73b2bbb815c55d6987417804ebd
Scott Caruso <scottcaruso@fullsail.edu>
2013-07-10 01:09:31 -0400

    Created base linear layout and layout params.

--- workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/BuildConfig.java
+++ workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/BuildConfig.java
@@ -1,6 +1,9 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.scottcaruso.java_i_1307_week_1;
 
+/**
+ * The Class BuildConfig.
+ */
 public final class BuildConfig {
     public final static boolean DEBUG = true;
 }
