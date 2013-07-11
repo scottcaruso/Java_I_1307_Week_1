df35f31c4c5bb3e0d5b320c308dfd061009c214e
Scott Caruso <scottcaruso@fullsail.edu>
2013-07-10 22:27:04 -0400

    Practicing with resource nodes.

--- workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/R.java
+++ workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/R.java
@@ -24,15 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f080000;
+    }
+    public static final class integer {
+        public static final int homeruns=0x7f040000;
+        public static final int rbis=0x7f040001;
+        public static final int steals=0x7f040002;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int baseballplayers=0x7f050003;
+        public static final int hello_world=0x7f050002;
+        public static final int playerone=0x7f050004;
+        public static final int playerthree=0x7f050006;
+        public static final int playertwo=0x7f050005;
     }
     public static final class style {
         /** 
@@ -56,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
