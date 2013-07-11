dad088cff12998478b40c0f67c851a4528ca5def
Scott Caruso <scottcaruso@fullsail.edu>
2013-07-11 03:33:31 -0400

    Creating APK file.

--- workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/BuildConfig.java
+++ workspace/java_i_1307_week_1/gen/com/scottcaruso/java_i_1307_week_1/BuildConfig.java
@@ -1,9 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.scottcaruso.java_i_1307_week_1;
 
-/**
- * The Class BuildConfig.
- */
 public final class BuildConfig {
     public final static boolean DEBUG = true;
 }
